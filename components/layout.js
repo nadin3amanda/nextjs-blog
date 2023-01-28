9a86b50e0a140ad69235ee6728aa0a8b654bf4ba
nadin3amanda <nadine@soar.earth>
2023-01-28 14:09:09 +0800

    first commit

--- components/layout.js
+++ components/layout.js
@@ -6,14 +6,14 @@ import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
-const name = '[Your Name]'
+const name = 'Nadine Amanda'
 export const siteTitle = 'Next.js Sample Website'
 
 export default function Layout({ children, home }) {
   return (
     <div className={styles.container}>
       <Head>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/nadine_favicon.png" />
         <meta
           name="description"
           content="Learn how to build a personal website using Next.js"
